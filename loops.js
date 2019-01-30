5a07e1f146662921df9a022673fe2740791e101a
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-30 23:22:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,10 @@
 function forLoop (array) {
-  forLoop = for (let i = 0; i < 25; i++) {
-  if (i == 0) {
+  for (let i = 0; i < 25; i++) {
+  if (i === 1) {
     array.push("I am a strange loop.")
   } else {
     array.push("I am ${i} strange loops.");
     }
   }
-  return forLoop;
-
+  return array;
 }
